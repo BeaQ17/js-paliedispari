1df23fd71c858ba3d7453e28e3407806c7d36fda
BeaQ17 <beaq235@gmail.com>
2021-04-29 19:52:08 +0200

    out of ideas to solve third part of second one

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,6 @@
 //PALINDROMA
 //Chiedi all'utente di inserire una parola
+/*
 var word = prompt("Inserisci una parola");
 
 isPalindrome(word);
@@ -23,5 +24,44 @@ function isPalindrome (word) {
 
     }
 }
+*/
+
+
+//PARI O DISPARI
+//L'utente sceglie pari o dispari e inserisce un numero da 1 a 5
+var choose = prompt("Pari o dispari?");
+console.log(choose);
+var num = prompt("Scegli un numero intero tra 1 e 5:");
+console.log(num);
+
+//fare una funzione per generare un numero random per il computer
+function randomNum(min, max) { 
+    min = Math.ceil(1);
+    max = Math.floor(5);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+} 
+
+console.log(randomNum(1, 5));
+
+
+//fare una funzione per sommare i due numeri e stabilire se la somma è pari o dispari
+//dichiarare chi ha vinto
+
+function isEven (num, randomNum) {
+    var sum = num + randomNum;
+    if (sum % 2 == 0, prompt == "pari") {
+        return "Vince giocatore";
+    } else if (sum % 2 != 0, prompt == "pari"){
+        return "Vince pc";
+    } else if (sum % 2 != 0, prompt == "dispari") {
+        return "Vince giocatore";
+    } else if (sum % 2 == 0, prompt == "dispari") {
+        return "Vince pc";
+    }
+         
+}
+
+console.log(isEven(num, randomNum));
+ 
 
 
